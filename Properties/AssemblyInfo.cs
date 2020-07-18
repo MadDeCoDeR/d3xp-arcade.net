13f431e1936a09bceac89fbaf0430d58074ab664
MadDeCoDeR <gkwin8ac@hotmail.gr>
2020-07-18 23:31:02 +0300

    Update Copyright

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("d3xp-arcadenet")]
-[assembly: AssemblyCopyright("Copyright(C) 2019 George Kalmpokis under MIT License")]
+[assembly: AssemblyCopyright("Copyright(C) 2020 George Kalmpokis under MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
